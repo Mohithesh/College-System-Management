9cb7d25b93dfeff17144396b78e2c6b49e931faf
Mohithesh H U <mohithesh46@gmail.com>
2024-07-07 14:10:47 +0530

    updated 07-07-2024

---
+++ style.css
@@ -0,0 +1,119 @@
+#3D52A0
+#7091E6
+#8697C4
+#ADBBDA
+#EDE8F5
+
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f9fcff;
+    background-image: linear-gradient(147deg, #f9fcff 0%, #7091E6 74%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 97.7vh;
+  }
+  
+  .container {
+    text-align: center;
+  }
+
+  .container h1 {
+    font-size: 60px;
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 600;
+    letter-spacing: 1px;
+    margin: -200px 0px 150px 0px;
+  }
+  
+  .select-role {
+    margin-bottom: 20px;
+  }
+  
+  .select-role h2 {
+    
+    font-optical-sizing: auto;
+    font-size: 60px;
+    font-weight: 500;
+    margin-bottom: 40px;
+  }
+  
+  .button-container {
+    display: flex;
+    justify-content: center;
+  }
+  
+  button {
+    background-color: #EDE8F5;
+    color: #282828;
+    border: none;
+    padding: 10px 20px;
+    font-size: 20px;
+    border-radius: 4px;
+    cursor: pointer;
+    margin: 0 10px;
+  }
+  
+  button:hover {
+    background-color: #ADBBDA;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  
+  .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .login-box {
+    display: none; /* Initially hide both login boxes */
+    background-color: #ffffff30;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin: 10px;
+    width: 300px;
+  }
+  
+  .login-box h2 {
+    font-family: "Montserrat", sans-serif;
+    font-size: 30px;
+    text-align: center;
+    margin-bottom: 40px;
+    color: #000000;
+  }
+  
+  .input-group {
+    margin-bottom: 15px;
+  }
+  
+  .input-group label {
+    display: block;
+    font-size: 20px;
+    text-align: left;
+    margin: 0px 0px 10px 10px;
+    color: #000000;
+  }
+  
+  .input-group input {
+    width: 95%;
+    padding: 8px;
+    margin: 0px 0px 10px 0px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 40px;
+  }
+  
+  button[type="submit"] {
+    width: 100%;
+    margin: 0px 0px 0px 0px;
+    border-radius: 40px;
+  }
+  
